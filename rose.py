532e0e1529ea081bba0e8388d1461e39310d9d85
giuseppe7 <giuseppe7@users.noreply.github.com>
2021-05-29 16:07:46 -0400

    First attempt at recursive printout.

--- rose.py
+++ rose.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Usage:
- rose <person>
+ rose <sAMAccountName>
 """
 
 from docopt import docopt
@@ -22,7 +22,53 @@ ENV_UNAME = 'ROSE_UNAME'
 ENV_PWORD = 'ROSE_PWORD'
 ENV_SEARCH_BASE = 'ROSE_SEARCH_BASE'
 
+SEARCH_ATTRS = [
+    'distinguishedName', 'sAMAccountName', 'accountExpires',
+    'objectClass', 'objectCategory',
+    'cn', 'name', 'title', 'mail', 'department', 'directReports', 'manager']
+# SEARCH_ATTRS = ['*']
+
+
 # Functions ..................................................................
+
+
+def get_person_dn(conn, basedn, sAMAccountName):
+    '''
+    Returns the person's DN given the connection, basedn, and sAMAccountName
+    '''
+    results = conn.search(
+        basedn,
+        "(&(objectClass=person)(sAMAccountName={}))".format(sAMAccountName),
+        attributes=SEARCH_ATTRS)
+
+    if results:
+        if len(conn.entries) > 1:
+            # Only expect one result.
+            raise Exception('Found more than one result.')
+        return conn.entries[0]
+    else:
+        raise Exception("No results found.")
+
+
+def print_person_and_directs(conn, basedn, targetdn, prefix):
+    print('{}{}'.format(prefix, targetdn.name))
+
+    if 'directReports' not in targetdn:
+        return
+
+    new_prefix = prefix + '    '
+    for directReport in targetdn.directReports:
+        results = conn.search(
+            search_base=directReport,
+            search_filter="(objectClass=*)",
+            search_scope=ldap3.BASE,
+            attributes=SEARCH_ATTRS)
+        if not results:
+            break
+        if int(conn.entries[0].accountExpires.value) <= 0:
+            break
+        print_person_and_directs(conn, basedn, conn.entries[0], new_prefix)
+
 
 # Main .......................................................................
 
@@ -32,7 +78,7 @@ def main():
     Convenient main method.
     '''
     arguments = docopt(__doc__)
-    target_person = arguments["<person>"]
+    target_person = arguments["<sAMAccountName>"]
 
     # Pull in host, port information from the environment variables.
     if ENV_HOST not in os.environ or ENV_PORT not in os.environ:
@@ -63,7 +109,7 @@ def main():
             port=target_port,
             use_ssl=True,
             tls=tls_config,
-            get_info=ldap3.ALL)
+            get_info=ldap3.NONE)
 
         c = ldap3.Connection(
                 s,
@@ -86,22 +132,10 @@ def main():
         print(err)
         sys.exit(1)
 
-    # Perform a basic search for now.
+    # Perform a basic search to obtain the DN of the given person.
     try:
-        search_attrs = [
-            'sAMAccountName', 'cn', 'name', 'title', 'mail',
-            'department', 'directReports', 'manager']
-        # search_attrs=['*']
-
-        results = c.search(
-            target_search_base,
-            "(&(objectClass=person)(sAMAccountName={}))".format(target_person),
-            attributes=search_attrs)
-
-        if results:
-            for e in c.entries:
-                print(e)
-
+        dn = get_person_dn(c, target_search_base, target_person)
+        print_person_and_directs(c, target_search_base, dn, "")
     except Exception as err:
         print(err)
         sys.exit(1)
